e39f14cd63088ace0e52e509fd98300344d0c756
Paul McGinley <paulmcginley16@outlook.com>
2026-04-09 19:39:19 +0100

    Added new test file for PricingRuleValidationTest.cs

---
+++ CheckoutKata.Tests/Validation/PricingRuleValidationTests.cs
@@ -0,0 +1,15 @@
+﻿using CheckoutKata.Models;
+
+namespace CheckoutKata.Tests.Validation
+{
+    public class PricingRuleValidationTests
+    {
+        [Fact]
+        public void pricingRule_WithNullItemCode_ShouldThrowException()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new PricingRule(null!, 50));
+        }
+
+    }
+}
