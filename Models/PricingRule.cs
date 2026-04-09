f0c66101f7a5b1a94f316716e192a7c1bd377d35
Paul McGinley <paulmcginley16@outlook.com>
2026-04-09 19:36:48 +0100

    Added Validation logic within Model/PricingRule

--- Models/PricingRule.cs
+++ Models/PricingRule.cs
@@ -20,18 +20,34 @@ namespace CheckoutKata.Models
         public int? SpecialPriceAmount { get; set; }
 
 
-        public PricingRule(string itemCode, int unitPrice, int? specialPriceQuantity, int? specialPriceAmount)
+        public PricingRule(string itemCode, int unitPrice, int? specialPriceQuantity = null, int? specialPriceAmount = null)
         {
+
+            if (string.IsNullOrWhiteSpace(itemCode))
+            {
+                throw new ArgumentException("Item code cannot be null or empty.", nameof(itemCode));
+            }
+
+            if(unitPrice <= 0)
+            {
+                throw new ArgumentException("Unit price cannot be negative.", nameof(unitPrice));
+            }
+
+            if(specialPriceQuantity.HasValue && specialPriceQuantity.Value <= 0)
+            {
+                throw new ArgumentException("Special price quantity must be greater than zero.", nameof(specialPriceQuantity));
+            }
+
+            if(specialPriceAmount.HasValue && specialPriceAmount.Value < 0)
+            {
+                throw new ArgumentException("Special price amount cannot be negative.", nameof(specialPriceAmount));
+            }
+
+
             this.ItemCode = itemCode;
             this.UnitPrice = unitPrice;
             this.SpecialPriceQuantity = specialPriceQuantity;
             this.SpecialPriceAmount = specialPriceAmount;
-        }
-
-        public PricingRule(string itemCode, int unitPrice)
-        {
-            this.ItemCode = itemCode;
-            this.UnitPrice = unitPrice;
         }
     }
 }
