6cb398f9a8c7f254333e9c29e432b94e0139d9b1
Paul McGinley <paulmcginley16@outlook.com>
2026-04-09 17:07:00 +0100

    Testing rename of file and boilerplate test method

---
+++ CheckoutKata.Tests/CheckoutValidationTests.cs
@@ -0,0 +1,11 @@
+namespace CheckoutKata.Tests
+{
+    public class CheckoutValidationTests
+    {
+        [Fact]
+        public void Checkout_NullPricingRules_ThrowsArgumentException()
+        {
+          // Testing commit check! 
+        }
+    }
+}
