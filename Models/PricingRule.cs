b060b0a35e142c54d8ddd845e31b90b9ae4deef4
Paul McGinley <paulmcginley16@outlook.com>
2026-04-09 11:30:58 +0100

    Added properties with constructor, added comments to make understanding more readable

--- Models/PricingRule.cs
+++ Models/PricingRule.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace CheckoutKata.Models
 {
-    internal class PricingRule
+    public class PricingRule
     {
+        // Note: SKU = Stock keeping units
+        public string ItemCode { get; set; }
+
+        public int UnitPrice { get; set; }
+
+        //Special offer price (e.g. 3 for 130)
+        public int? SpecialPriceQuantity { get; set; }
+
+        // Total price for speical offer quantity
+        public int? SpecialPriceAmount { get; set; }
+
+        
+        public PricingRule(string ItemCode, int unitPrice, int? specialPriceQuantity, int? specialPriceAmount) 
+        {
+            this.ItemCode = ItemCode;
+            this.UnitPrice = unitPrice;
+            this.SpecialPriceQuantity = specialPriceQuantity;
+            this.SpecialPriceAmount = specialPriceAmount;
+        }
+
+
     }
 }
