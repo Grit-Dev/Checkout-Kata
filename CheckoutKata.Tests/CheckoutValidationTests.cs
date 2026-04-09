44a1c43161d679fe3b6c21eb73814fff11f83292
Paul McGinley <paulmcginley16@outlook.com>
2026-04-09 17:39:20 +0100

    Additional tests - One failed - will implement validation protection around this

--- CheckoutKata.Tests/CheckoutValidationTests.cs
+++ CheckoutKata.Tests/CheckoutValidationTests.cs
@@ -28,7 +28,7 @@ namespace CheckoutKata.Tests
         }
 
         [Fact]
-        public void Checkout_NullPricingRules_ThrowsArgumentException()
+        public void ScanSku_WithEmptyString_ThrowsArgumentException()
         {
             // Arrange
             var checkout = new Checkout(new List<PricingRule>());
@@ -37,28 +37,46 @@ namespace CheckoutKata.Tests
             Assert.Throws<ArgumentException>(() => checkout.ScanSku(""));
         }
 
-        //[Fact]
-        //public void Checkout_NullPricingRules_ThrowsArgumentException()
-        //{
-        //    // Arrange
-        //    // Act
-        //    // Assert
-        //}
+        [Fact]
+        public void ScanSku_WithWhiteSpace_ShouldThrowException()
+        {
+            // Arrange
+            var checkout = new Checkout(new List<PricingRule>());
 
-        //[Fact]
-        //public void Checkout_NullPricingRules_ThrowsArgumentException()
-        //{
-        //    // Arrange
-        //    // Act
-        //    // Assert
-        //}
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => checkout.ScanSku("    "));
+        }
 
-        //[Fact]
-        //public void Checkout_NullPricingRules_ThrowsArgumentException()
-        //{
-        //    // Arrange
-        //    // Act
-        //    // Assert
-        //}
+        [Fact]
+        public void ScanSku_WithValidSku_ShouldNotThrowException()
+        {
+            // Arrange
+            var checkout = new Checkout(new List<PricingRule>());
+            
+            // Act
+            checkout.ScanSku("A");
+            
+            // Assert
+            Assert.True(true);
+        }
+
+        //  Assert.Throws() Failure: No exception was thrown
+        // Expected: typeof(System.ArgumentException)
+        // - Hmm come back to and add protection around this. 
+            [Fact]
+        public void ScanSku_WithUnknownSku_ShouldThrowException()
+        {
+            // Arrange
+            var rules = new List<PricingRule>
+            {
+                new PricingRule("A", 50),
+                new PricingRule("B", 30)
+            };
+
+            var checkout = new Checkout(rules);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => checkout.ScanSku("x"));
+        }
     }
 }
